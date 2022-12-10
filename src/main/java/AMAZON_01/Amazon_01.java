f2e9f205a406e1aa352b603c19244d41563d0164
Atefeh686 <atefehghalenoee812@gmail.com>
2022-12-10 10:42:52 -0500

    first commit

---
+++ src/main/java/AMAZON_01/Amazon_01.java
@@ -0,0 +1,24 @@
+package AMAZON_01;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Amazon_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.amazon.com/");
+		driver.manage().window().maximize();
+
+
+
+
+	
+
+	}
+
+}
